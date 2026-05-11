ced9c828d1627edc1aa47d62f9b5ed6e16f9bb85
fiexinn <natelandin26@gmail.com>
2026-05-11 18:01:51 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/det825-i5-cyber-site/', 
   server: {
     host: "::",
     port: 8080,
